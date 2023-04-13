e9dbe02019fefff21d472fe621db8d82ac4b5041
wintory <willyvongga@hotmail.com>
2023-04-13 21:25:27 +0700

    build: add global style

--- packages/web/src/Provider.tsx
+++ packages/web/src/Provider.tsx
@@ -1,13 +1,18 @@
 import { ThemeProvider } from '@mui/material';
+import { GlobalStyles } from '@mui/material';
 import { FC } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { getGlobalStyle } from './styles/globalStyle';
 import theme from './styles/theme';
 
 const Provider: FC = ({ children }) => {
   return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>{children}</BrowserRouter>
-    </ThemeProvider>
+    <>
+      <GlobalStyles styles={getGlobalStyle(theme)} />
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>{children}</BrowserRouter>
+      </ThemeProvider>
+    </>
   );
 };
 
