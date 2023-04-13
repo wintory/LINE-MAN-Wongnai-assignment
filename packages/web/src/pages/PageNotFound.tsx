47287ec11578b29bbe834254eecb42288e2c8cf1
wintory <willyvongga@hotmail.com>
2023-04-13 22:05:45 +0700

    build: add page not found

--- packages/web/src/pages/PageNotFound.tsx
+++ packages/web/src/pages/PageNotFound.tsx
@@ -1,5 +1,6 @@
-import { styled, Typography } from '@mui/material';
+import { Button, styled, Typography } from '@mui/material';
 import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const PageWrapper = styled('div')(() => ({
   width: '100vw',
@@ -10,18 +11,28 @@ const PageWrapper = styled('div')(() => ({
 }));
 
 const PageNotFoundBox = styled('div')(() => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  display: 'block',
+  textAlign: 'center',
 }));
 
 const PageNotFound: FC = () => {
+  const navigate = useNavigate();
+  const handleBackToHome = () => {
+    navigate('/');
+  };
+
   return (
     <PageWrapper>
       <PageNotFoundBox>
-        <Typography variant="h4" color="common.black">
+        <Typography variant="h2" color="common.black">
           404 Page not found
         </Typography>
+        <Typography variant="h4" pt={1} mb={2}>
+          Sorry, the page you're looking for doesn't exists.
+        </Typography>
+        <Button color="secondary" onClick={handleBackToHome}>
+          <Typography variant="h4">Back to Home</Typography>
+        </Button>
       </PageNotFoundBox>
     </PageWrapper>
   );
