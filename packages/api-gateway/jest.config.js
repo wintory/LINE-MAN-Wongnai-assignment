ddc85b3cb00ac5bb83aee866847e87fda6aa1caf
wintory <willyvongga@hotmail.com>
2023-04-17 22:38:33 +0700

    build: add more lib

--- packages/api-gateway/jest.config.js
+++ packages/api-gateway/jest.config.js
@@ -4,8 +4,4 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   moduleDirectories: ['node_modules', 'src'],
-  moduleNameMapper: {
-    '^@components$': '<rootDir>/shared/components/',
-    '^@assets$': '<rootDir>/shared/assets/',
-  },
 };
